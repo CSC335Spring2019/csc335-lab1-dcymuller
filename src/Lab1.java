89baf5183adc5f20c36ca0a3ec1b72c99f10e2eb
dcymuller <dmuller@email.arizona.edu>
2019-01-16 09:17:50 -0700

    first commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,6 +1,7 @@
 /**
  * 
- * @author YOUR NAME HERE
+ * @author Dyana Muller
+ * Jan 16, 2019
  * 
  * 
  */
@@ -8,7 +9,7 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello, CSC 335, I'm Dyana!");
 	}
 
 }
